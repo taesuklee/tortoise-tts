ab485fe1b72412f0953d2b1712dc6c3b434c6bd8
taesuklee <taesuk.lee.data@tuta.io>
2023-06-16 20:14:14 +0200

    results in output folder

--- tortoise/do_tts.py
+++ tortoise/do_tts.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     parser.add_argument('--voice', type=str, help='Selects the voice to use for generation. See options in voices/ directory (and add your own!) '
                                                  'Use the & character to join two voices together. Use a comma to perform inference on multiple voices.', default='random')
     parser.add_argument('--preset', type=str, help='Which voice preset to use.', default='fast')
-    parser.add_argument('--output_path', type=str, help='Where to store outputs.', default='results/')
+    parser.add_argument('--output_path', type=str, help='Where to store outputs.', default='outputs/')
     parser.add_argument('--model_dir', type=str, help='Where to find pretrained model checkpoints. Tortoise automatically downloads these to .models, so this'
                                                       'should only be specified if you have custom checkpoints.', default=MODELS_DIR)
     parser.add_argument('--candidates', type=int, help='How many output candidates to produce per-voice.', default=3)
